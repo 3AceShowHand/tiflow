1b08c1f686f3217bc65587067eaf8a94391b33bc
3AceShowHand <jinl1037@hotmail.com>
2022-07-28 21:33:02 +0800

    fix owner resign error check.

--- cdc/capture/capture.go
+++ cdc/capture/capture.go
@@ -465,11 +465,11 @@ func (c *captureImpl) campaignOwner(ctx cdcContext.Context) error {
 		// use a new context to prevent the context from being cancelled.
 		resignCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		if resignErr := c.resign(resignCtx); resignErr != nil {
-			if errors.Cause(err) != context.DeadlineExceeded {
+			if errors.Cause(resignErr) != context.DeadlineExceeded {
 				log.Info("owner resign failed", zap.String("captureID", c.info.ID),
-					zap.Error(err), zap.Int64("ownerRev", ownerRev))
+					zap.Error(resignErr), zap.Int64("ownerRev", ownerRev))
 				cancel()
-				return errors.Trace(err)
+				return errors.Trace(resignErr)
 			}
 
 			log.Warn("owner resign timeout", zap.String("captureID", c.info.ID),
