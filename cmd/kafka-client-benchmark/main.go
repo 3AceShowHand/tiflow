a2b41b17bed31677df9427906fb6fcffe73bbd98
3AceShowHand <jinl1037@hotmail.com>
2023-02-28 19:37:26 +0800

    add new

--- cmd/kafka-client-benchmark/main.go
+++ cmd/kafka-client-benchmark/main.go
@@ -10,9 +10,7 @@ import (
 
 	"github.com/pingcap/log"
 	"github.com/pingcap/tiflow/cdc/model"
-	"github.com/pingcap/tiflow/pkg/sink/codec/common"
 	"github.com/pingcap/tiflow/pkg/sink/kafka"
-	v2 "github.com/pingcap/tiflow/pkg/sink/kafka/v2"
 	"go.uber.org/zap"
 )
 
@@ -23,8 +21,8 @@ func main() {
 	option.BrokerEndpoints = []string{"127.0.0.1:9092"}
 	option.ClientID = "kafka-client"
 
-	factory, err := v2.NewFactory(option, changefeed)
-	// factory, err := kafka.NewSaramaFactory(option, changefeed)
+	// factory, err := v2.NewFactory(option, changefeed)
+	factory, err := kafka.NewSaramaFactory(option, changefeed)
 	if err != nil {
 		log.Error("create kafka factory failed", zap.Error(err))
 		return
@@ -50,60 +48,75 @@ func main() {
 		wg       sync.WaitGroup
 	)
 
-	events := make(chan []*common.Message, 1024)
-
 	wg.Add(1)
-	ticker := time.NewTicker(15 * time.Millisecond)
 	go func() {
-		defer func() {
-			ticker.Stop()
-			wg.Done()
-		}()
 		for {
-			select {
-			case <-ctx.Done():
+			rand.Read(value)
+			err := asyncProducer.AsyncSend(ctx, topic, 0, key, value, func() {
+				atomic.AddUint64(&ackTotal, 1)
+			})
+			if err != nil {
+				log.Error("send kafka message failed", zap.Error(err))
 				return
-			case <-ticker.C:
-				rand.Read(value)
-				messages := make([]*common.Message, 0, 1024)
-				for i := 0; i < 1024; i++ {
-					messages = append(messages, &common.Message{
-						Key:   key,
-						Value: value,
-						Callback: func() {
-							atomic.AddUint64(&ackTotal, 1)
-						},
-					})
-				}
-				select {
-				case <-ctx.Done():
-					return
-				case events <- messages:
-				}
 			}
+			atomic.AddUint64(&total, 1)
 		}
 	}()
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case messages := <-events:
-				err := asyncProducer.AsyncSendMessages(ctx, topic, 0, messages)
-				if err != nil {
-					log.Error("send kafka message failed",
-						zap.Int("count", len(messages)),
-						zap.Error(err))
-					return
-				} else {
-					atomic.AddUint64(&total, uint64(len(messages)))
-				}
-			}
-		}
-	}()
+	//events := make(chan []*common.Message, 1024)
+	//
+	//wg.Add(1)
+	//ticker := time.NewTicker(15 * time.Millisecond)
+	//go func() {
+	//	defer func() {
+	//		ticker.Stop()
+	//		wg.Done()
+	//	}()
+	//	for {
+	//		select {
+	//		case <-ctx.Done():
+	//			return
+	//		case <-ticker.C:
+	//			rand.Read(value)
+	//			messages := make([]*common.Message, 0, 1024)
+	//			for i := 0; i < 1024; i++ {
+	//				messages = append(messages, &common.Message{
+	//					Key:   key,
+	//					Value: value,
+	//					Callback: func() {
+	//						atomic.AddUint64(&ackTotal, 1)
+	//					},
+	//				})
+	//			}
+	//			select {
+	//			case <-ctx.Done():
+	//				return
+	//			case events <- messages:
+	//			}
+	//		}
+	//	}
+	//}()
+	//
+	//wg.Add(1)
+	//go func() {
+	//	defer wg.Done()
+	//	for {
+	//		select {
+	//		case <-ctx.Done():
+	//			return
+	//		case messages := <-events:
+	//			err := asyncProducer.AsyncSendMessages(ctx, topic, 0, messages)
+	//			if err != nil {
+	//				log.Error("send kafka message failed",
+	//					zap.Int("count", len(messages)),
+	//					zap.Error(err))
+	//				return
+	//			} else {
+	//				atomic.AddUint64(&total, uint64(len(messages)))
+	//			}
+	//		}
+	//	}
+	//}()
 
 	wg.Add(1)
 	go func() {
