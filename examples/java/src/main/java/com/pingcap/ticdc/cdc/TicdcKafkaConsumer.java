1b184aef4f797964f89c536191f6821fd55a1d83
3AceShowHand <jinl1037@hotmail.com>
2023-06-26 18:29:01 +0800

    kafka consumer can run.

--- examples/java/src/main/java/com/pingcap/ticdc/cdc/TicdcKafkaConsumer.java
+++ examples/java/src/main/java/com/pingcap/ticdc/cdc/TicdcKafkaConsumer.java
@@ -9,7 +9,10 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 
 import org.apache.avro.generic.GenericRecord;
 
-import java.util.Arrays;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
 import java.util.Properties;
 
 public class TicdcKafkaConsumer {
@@ -21,19 +24,19 @@ public class TicdcKafkaConsumer {
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "group1");
 
 
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroDeserializer");
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroDeserializer");
         props.put("schema.registry.url", "http://localhost:8081");
 
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        String topic = "avro-test";
+        String topic = "avro-checksum-test";
         final Consumer<String, GenericRecord> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Arrays.asList(topic));
+        consumer.subscribe(Collections.singletonList(topic));
 
         try {
             while (true) {
-                ConsumerRecords<String, GenericRecord> records = consumer.poll(100);
+                ConsumerRecords<String, GenericRecord> records = consumer.poll(Duration.ofMillis(100));
                 for (ConsumerRecord<String, GenericRecord> record : records) {
                     System.out.printf("offset = %d, key = %s, value = %s \n", record.offset(), record.key(), record.value());
                 }
@@ -42,5 +45,4 @@ public class TicdcKafkaConsumer {
             consumer.close();
         }
     }
-
 }
