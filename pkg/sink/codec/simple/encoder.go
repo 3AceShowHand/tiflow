65700c37b63fca47eb46cdbe567ce270a0b7fb86
3AceShowHand <jinl1037@hotmail.com>
2023-12-25 12:00:52 +0800

    use sonic.

--- pkg/sink/codec/simple/encoder.go
+++ pkg/sink/codec/simple/encoder.go
@@ -16,7 +16,8 @@ package simple
 import (
 	"context"
 	//"encoding/json"
-	"github.com/goccy/go-json"
+	//"github.com/goccy/go-json"
+	json "github.com/bytedance/sonic"
 
 	"github.com/pingcap/errors"
 	"github.com/pingcap/log"
