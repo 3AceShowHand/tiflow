088a313ea242a02ec1a800a692a51d9bc738a128
3AceShowHand <jinl1037@hotmail.com>
2024-10-12 13:43:03 +0800

    remove max messages from the kafka options

--- pkg/sink/kafka/options.go
+++ pkg/sink/kafka/options.go
@@ -156,9 +156,6 @@ type Options struct {
 	Compression       string
 	ClientID          string
 	RequiredAcks      RequiredAcks
-	// Only for test. User can not set this value.
-	// The current prod default value is 0.
-	MaxMessages int
 
 	// Credential is used to connect to kafka cluster.
 	EnableTLS          bool
