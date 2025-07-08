81b22b8e9f5684ce4744ce5bf2e65b510de9834e
3AceShowHand <jinl1037@hotmail.com>
2025-07-08 17:16:32 +0800

    set to 200 ms

--- cdc/processor/sourcemanager/engine/pebble/event_sorter.go
+++ cdc/processor/sourcemanager/engine/pebble/event_sorter.go
@@ -469,7 +469,7 @@ func (s *EventSorter) handleEvents(
 					zap.String("namespace", s.changefeedID.Namespace),
 					zap.String("changefeed", s.changefeedID.ID))
 			}
-			time.Sleep(50 * time.Millisecond)
+			time.Sleep(200 * time.Millisecond)
 			writeDuration.Observe(time.Since(start).Seconds())
 			batch = db.NewBatch()
 		}
